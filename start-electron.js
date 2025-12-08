6d98f47c3c8080f84b05fb93ae611ae6be4e428b
lu7ue <zhan0067@hz.nl>
2025-12-08 17:48:53 +0100

    run commands depends on operating system

---
+++ start-electron.js
@@ -0,0 +1,19 @@
+const { spawn } = require('child_process');
+const waitOn = require('wait-on');
+
+waitOn({ resources: ['http://localhost:5173'] }, (err) => {
+    if (err) {
+        console.error('Vite did not start in time:', err);
+        process.exit(1);
+    }
+
+    console.log('Vite ready, starting Electron...');
+
+    const cmd = process.platform === 'win32'
+        ? 'node_modules\\.bin\\electron.cmd'
+        : 'node_modules/.bin/electron';
+
+    const proc = spawn(cmd, ['.'], { stdio: 'inherit', shell: true });
+
+    proc.on('close', (code) => process.exit(code));
+});
